02ca8dec456c934499f5d6be1c774076b324728b
Marcus Rickert <marcus.rickert@web.de>
2016-01-22 11:16:37 +0100

    introduce log_stat to preserve log state across reload()

--- log.py
+++ log.py
@@ -6,24 +6,21 @@ import os
 import shutil
 import json
 
+import log_stat
+
 LOGGING_FILENAME = "etc/log_config.json"
 LOGGING_TEMPLATE_FILENAME = "etc/log_config_template.json"
-
-global logging_config_loaded
-
-logging_config_loaded = False
 
 def open_logging(module_name, reload = False):
   
   global logger
-  global logging_config_loaded
   
-  if reload or not logging_config_loaded:
-    
+  if reload or not log_stat.get_log_started():
+
+    log_stat.set_log_started(True)    
     copy_template = os.path.exists(LOGGING_TEMPLATE_FILENAME) and not os.path.exists(LOGGING_FILENAME)
     if copy_template:
       shutil.copyfile(LOGGING_TEMPLATE_FILENAME, LOGGING_FILENAME)
-    logging_config_loaded = True
     logging_config_json_file = open(LOGGING_FILENAME)
     parsed_logging_data = json.load(logging_config_json_file)
     logging.config.dictConfig(parsed_logging_data)
